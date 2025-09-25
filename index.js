c54f21ae4461ce017697e68b0394975694bb7d7d
yLorde <davisouza2204@gmail.com>
2025-09-25 16:59:10 -0300

    melhorando a estruturação

--- index.js
+++ index.js
@@ -1,67 +1,69 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 (async function () {
+  const Options = {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.99 Mobile/15E148 Safari/604.1",
+    },
+  };
+
+  const titleName = "bruxa de blair";
+
+  async function findLink(query) {
     try {
+      const url = `https://www.themoviedb.org/search?query=${encodeURIComponent(
+        query
+      )}`;
+      const { data } = await axios.get(url, Options);
+      const $ = cheerio.load(data);
 
-        const title = "bruxa de blair";
-        const base_url = 'https://www.themoviedb.org';
+      // procura o primeiro link de resultado
+      const link = $(".results .card .title a").first().attr("href");
 
-        let image;
-        let link;
-        let sinopse;
-        let background;
-
-        async function findLink() {
-            await axios.get(`${base_url}/search?query=${String(title).replaceAll(' ', '%20')}`, {
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.99 Mobile/15E148 Safari/604.1"
-                }
-            }).catch((err) => {
-                if (err) {
-                    return;
-                };
-            }).then(response => {
-                if (response) {
-                    const $ = cheerio.load(response.data);
-                    link = $('div .results').find('div .card').find('div .wrapper').find('div .details').find('div .title').find('div').find('a').attr('href');
-                };
-            });
-        };
-
-        async function findBanner() {
-            await axios.get(`${base_url}${link}`, {
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.99 Mobile/15E148 Safari/604.1"
-                }
-            }).catch((err) => {
-                if (err) {
-                    return;
-                };
-            }).then(response => {
-                if (response) {
-                    const $ = cheerio.load(response.data);
-
-                    image = $('main').find('section').find('div .poster').find('img').attr('srcset').split(',')[1].toString().split(' ')[1].replace(' x2', '');
-                    sinopse = $('main').find('section').find('div .header_info').find('div .overview').find('p').text();
-                    background = $('section .media').find('div .backdrop').find('img').attr('srcset').split(',')[1].toString().split(' ')[1].replace(' x2', '');
-                };
-            });
-        };
-
-        await findLink();
-        await findBanner();
-
-        const data = {
-            image: image,
-            sinopse: sinopse,
-            background: background,
-            link_1: `${base_url}/search?query=${String(title).replaceAll(' ', '%20')}`,
-            link_2: `${base_url}${link}`,
-        };
-
-        console.log(data);
+      if (!link) throw new Error("Nenhum resultado encontrado.");
+      return { link, searchUrl: url };
     } catch (err) {
-        console.log(err);
-    };
-})()
+      throw new Error("Erro ao buscar filme: " + err.message);
+    }
+  }
+
+  async function findDetails(link) {
+    try {
+      const url = `https://www.themoviedb.org${link}`;
+      const { data } = await axios.get(url, Options);
+      const $ = cheerio.load(data);
+
+      // Poster
+      let image = $("main section .poster img").attr("src");
+      if (image && image.startsWith("/")) {
+        image = "https://www.themoviedb.org" + image;
+      }
+
+      // Sinopse
+      const sinopse = $("main section .header_info .overview p")
+        .text()
+        .trim();
+
+      // Background
+      let background = $("section .media .backdrop img").attr("src");
+      if (background && background.startsWith("/")) {
+        background = "https://www.themoviedb.org" + background;
+      }
+
+      return { image, sinopse, background, detailsUrl: url };
+    } catch (err) {
+      throw new Error("Erro ao buscar detalhes: " + err.message);
+    }
+  }
+
+  try {
+    const { link, searchUrl } = await findLink(titleName);
+    const result = await findDetails(link);
+
+    console.log(result);
+  } catch (err) {
+    console.error(err.message);
+  }
+})();
